7a955000dad46822b6d8517ff83f015be4a303ee
Marcello Barnaba <vjt@openssl.it>
2009-08-23 17:21:45 +0200

    Added the "Kitsch" theme (quite horrible ;)

---
+++ themes/kitsch.js
@@ -0,0 +1,15 @@
+Speedometer.themes.kitsch = {
+  dial: 'Green',
+  rim: 'Purple',
+  thresh: 'Blue',
+  center: 'Blue',
+  nose: 'Yellow',
+  hand: 'Blue',
+  handShine: 'LightBlue',
+  handShineTo: 'Blue',
+  ticks: 'Purple',
+  marks: 'White',
+  strings: 'Red',
+  digits: 'Green',
+  font: 'Times'
+}
